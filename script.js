989b5a13c065a0a736e70dd2324ff5fb1bf2d765
Ozbork97 <reynaldoa1970628@gmail.com>
2025-07-29 20:58:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const mensaje = `
+Mi querida Dayi,
+
+He estado pensando en ti todos estos días, y la verdad es que me has hecho falta… igual o incluso más que antes. Te pienso aunque a veces intente no hacerlo. Y aunque estemos en esta sitiacón, no hay un solo segundo en el que no imagine cómo recuperarte, cómo volver a encender esa llama de AMOR.
+
+En los últimos años, mi vida y mis sueños han tenido tu nombre escrito por todas partes. Y aunque mi forma de ser no siempre fue la mejor, mi intención siempre fue clara: hacerte feliz, crecer a tu lado, sin detenerte ni ser una carga para ti.
+
+Hoy te escribo esto porque hay cosas que no logro decir por chat. Tal vez por miedo, tal vez por no querer sufrir más. Pero lo que sí puedo decirte sin temblar, es que te amo con todo mi corazón.
+
+Esta carta es de tu chino, el que te ama con el alma, y que nunca, NUNCA dejó de creer en nosotros.
+
+Si lees esto solo escribeme el siguiente mensaje: Vamos ha hacerlo sin miedo y vamos a darlo todo.
+`;
+
+document.getElementById("mensaje").innerText = mensaje;
